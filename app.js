9a50fdca9efc73416d9d5c4d1d616951c8d1d36c
Alexander <91787768+Pixlent@users.noreply.github.com>
2025-11-12 18:28:22 +0100

    feat: add 10 pictures slideshow

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ document.addEventListener("mousemove", function (event) {
   let mouseX = event.clientX - paddingX;
   let mouseY = event.clientY - paddingY;
 
-  // Manually update particles.js mouse position parameters
-  // Assuming you have access to the particlesJS instance or its config
   window.pJSDom[0].pJS.interactivity.mouse.pos_x = mouseX;
   window.pJSDom[0].pJS.interactivity.mouse.pos_y = mouseY;
   window.pJSDom[0].pJS.interactivity.status = "mousemove";
@@ -44,25 +42,97 @@ interactiveElements.forEach((el) => {
 });
 
 document.getElementById("boy-in-the-bubble-button").onclick = function () {
-  document.getElementById("boy-in-the-bubble-video").style.visibility =
-    "visible";
-  document.getElementById("close-button").style.visibility = "visible";
-  document.getElementById("particles-js").style.visibility = "hidden";
+  openFullscreenContentById("boy-in-the-bubble-video");
 };
 
 document.getElementById("farm-documentary-button").onclick = function () {
-  document.getElementById("farm-documentary-video").style.visibility =
-    "visible";
-  document.getElementById("close-button").style.visibility = "visible";
-  document.getElementById("particles-js").style.visibility = "hidden";
+  openFullscreenContentById("farm-documentary-video");
 };
+
+document.getElementById("ten-pictures-button").onclick = function () {
+  openFullscreenContentByClassName("ten-pictures");
+};
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight" || event.code === "KeyD") {
+    tenPicturesNext();
+  }
+  if (event.key === "ArrowLeft" || event.code === "KeyA") {
+    tenPicturesPrevious();
+  }
+  if (event.key === "Escape") {
+    closeFullscreenContent();
+  }
+});
 
 const fullscreenContent = document.querySelectorAll(".fullscreen-content");
 
-document.getElementById("close-button").onclick = function () {
+function openFullscreenContentByClassName(content) {
+  openFullscreenContent(document.getElementsByClassName(content)[0]);
+}
+
+function openFullscreenContentById(content) {
+  openFullscreenContent(document.getElementById(content));
+}
+
+function openFullscreenContent(content) {
+  content.style.visibility = "visible";
+  document.getElementById("close-button").style.visibility = "visible";
+  document.getElementById("particles-js").style.visibility = "hidden";
+  document.getElementById("content").style.visibility = "hidden";
+}
+
+function closeFullscreenContent() {
   fullscreenContent.forEach((element) => {
     element.style.visibility = "hidden";
   });
   document.getElementById("close-button").style.visibility = "hidden";
   document.getElementById("particles-js").style.visibility = "visible";
+  document.getElementById("content").style.visibility = "visible";
+}
+
+document.getElementById("close-button").onclick = closeFullscreenContent;
+
+let pictureIndex = 0;
+const maxPictureIndex = 9;
+
+document.getElementById("ten-pictures-button-previous").onclick = function () {
+  tenPicturesPrevious();
+  console.log("clicked button");
 };
+document.getElementById("ten-pictures-button-next").onclick = tenPicturesNext;
+
+function tenPicturesPrevious() {
+  pictureIndex = Math.max(-1, pictureIndex - 1);
+  if (pictureIndex == -1) {
+    pictureIndex = maxPictureIndex;
+  }
+  animateDirection(pictureIndex, "-5");
+  displayPicture(pictureIndex);
+}
+
+function tenPicturesNext() {
+  pictureIndex = Math.min(maxPictureIndex + 1, pictureIndex + 1);
+  if (pictureIndex == maxPictureIndex + 1) {
+    pictureIndex = 0;
+  }
+  animateDirection(pictureIndex, "5");
+  displayPicture(pictureIndex);
+}
+
+function displayPicture(index) {
+  const visiblePicture = document.getElementsByClassName("picture-visible")[0];
+  const newVisiblePicture = document.getElementById("pictures").children[index];
+
+  visiblePicture.classList.remove("picture-visible");
+  visiblePicture.classList.add("picture-hidden");
+
+  newVisiblePicture.classList.remove("picture-hidden");
+  newVisiblePicture.classList.add("picture-visible");
+}
+
+function animateDirection(index, direction) {
+  const element = document.getElementById("pictures").children[index];
+  element.style.setProperty("--translate-x", direction + "vh");
+  void element.offsetWidth;
+}
